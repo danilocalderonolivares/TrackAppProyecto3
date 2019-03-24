36b558b8e8c5ccd2a52738c3d9686f4f65f5b667
Jose Alonso <jmadrigal_94@outlook.com>
2019-03-24 12:51:33 -0600

    Se corrigen las rutas de los componentes auxiliares del chat

--- src/main/webapp/app/webcustom/chat/chat.module.ts
+++ src/main/webapp/app/webcustom/chat/chat.module.ts
@@ -2,10 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { GpsAppSharedModule } from 'app/shared';
 import { ChatComponent } from 'app/webcustom/chat/chat.component';
 import { GpsAppChatRouting } from 'app/webcustom/chat/chat.route';
+import { ChatsListComponent } from 'app/webcustom/chat/chats-list/chats-list.component';
+import { ChatWindowComponent } from 'app/webcustom/chat/chat-window/chat-window.component';
 
 @NgModule({
     imports: [GpsAppSharedModule, GpsAppChatRouting],
-    declarations: [ChatComponent],
+    declarations: [ChatComponent, ChatsListComponent, ChatWindowComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GpsAppChatModule {}
