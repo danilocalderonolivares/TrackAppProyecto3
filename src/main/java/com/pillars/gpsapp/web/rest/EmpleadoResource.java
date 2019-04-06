320f23a8ea9e0813b5841ede44ce5b0f2905152d
Jose <jmadrigal_94@outlook.com>
2019-04-05 18:41:17 -0600

    Se termina metodo requerido para la comunicacion entre el simulador y el backend de la aplicacion

--- src/main/java/com/pillars/gpsapp/web/rest/EmpleadoResource.java
+++ src/main/java/com/pillars/gpsapp/web/rest/EmpleadoResource.java
@@ -7,19 +7,16 @@ import com.pillars.gpsapp.repository.EmpleadoRepository;
 import com.pillars.gpsapp.repository.UserRepository;
 import com.pillars.gpsapp.web.rest.errors.BadRequestAlertException;
 import com.pillars.gpsapp.web.rest.util.HeaderUtil;
-import com.pillars.gpsapp.web.rest.vm.LoginVM;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
-import java.util.stream.Stream;
 
 /**
  * REST controller for managing Empleado.
