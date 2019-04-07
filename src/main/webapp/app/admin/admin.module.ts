facb2bf294ec67b032fac8657ff4eb16a7fe8ce1
Rolando Conejo Vargas <rconejov@ucenfotec.ac.cr>
2019-04-07 00:04:58 -0600

    Initial application generated by JHipster-5.8.2

--- src/main/webapp/app/admin/admin.module.ts
+++ src/main/webapp/app/admin/admin.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { JhiLanguageService } from 'ng-jhipster';
+import { JhiLanguageHelper } from 'app/core';
 import { GpsAppSharedModule } from 'app/shared';
 /* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
 
@@ -15,7 +17,8 @@ import {
     JhiHealthModalComponent,
     JhiHealthCheckComponent,
     JhiConfigurationComponent,
-    JhiDocsComponent
+    JhiDocsComponent,
+    JhiTrackerComponent
 } from './';
 
 @NgModule({
@@ -35,9 +38,19 @@ import {
         JhiHealthCheckComponent,
         JhiHealthModalComponent,
         JhiDocsComponent,
+        JhiTrackerComponent,
         JhiMetricsMonitoringComponent
     ],
+    providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
     entryComponents: [UserMgmtDeleteDialogComponent, JhiHealthModalComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class GpsAppAdminModule {}
+export class GpsAppAdminModule {
+    constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
+        this.languageHelper.language.subscribe((languageKey: string) => {
+            if (languageKey !== undefined) {
+                this.languageService.changeLanguage(languageKey);
+            }
+        });
+    }
+}
